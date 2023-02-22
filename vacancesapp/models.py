dc2d83bcb3a602065017a47d0149468a1667fbe0
Hagryn <loic.guyaz@gmail.com>
2023-02-22 17:49:47 +0100

    feat: Add 9 Pokemon to database in a file to load and also pokemont types (#3)

--- vacancesapp/models.py
+++ vacancesapp/models.py
@@ -15,21 +15,15 @@ class PokemonType(models.Model):
     max_happiness = models.PositiveIntegerField()
     cost = models.PositiveIntegerField()
     cash_factor = models.FloatField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
 class Pokemon(models.Model):
     pokemon_type = models.ForeignKey(PokemonType, related_name="pokemons", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     obtainable = models.BooleanField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
 class Area(models.Model):
     name = models.CharField(max_length=100)
     size = models.PositiveIntegerField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
 class OwnedPokemon(models.Model):
     pokemon = models.ForeignKey(Pokemon, null=True, related_name="owned_pokemons", on_delete=models.SET_NULL)
