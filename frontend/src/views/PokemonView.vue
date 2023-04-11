4e107a9a4c5d963b227703d2cbc5b1ccefb7211c
Jonas Allemann <jonas.allemann@he-arc.ch>
2023-04-11 14:07:04 +0200

    feat: add colorisation on owned pokemon in pokedex

--- frontend/src/views/PokemonView.vue
+++ frontend/src/views/PokemonView.vue
@@ -8,24 +8,18 @@ let success;
 let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 if (urlParams.has("success")) {
-  success = urlParams.get('success');
+  success = urlParams.get("success");
 }
 
-
-
 const fetchPokemons = async () => {
-  const result = await axios.get(
-    "pokemons/"
-  );
+  const result = await axios.get("pokemons/pokemons_of_user/");
   pokemons.value = result.data;
 };
 
 const pokemonTypes = ref([]);
 
 const fetchPokemonTypes = async () => {
-  pokemonTypes.value = (
-    await axios.get("pokemon-types/")
-  ).data;
+  pokemonTypes.value = (await axios.get("pokemon-types/")).data;
 };
 
 const removePokemon = async (id) => {
@@ -45,14 +39,14 @@ onMounted(() => {
 <template>
   <q-page>
     <h1>Pokédex</h1>
-    
+
     <div class="text-center q-mb-md q-mt-md">
       <q-btn color="green" :to="{ name: 'pokemons.create' }">
         <q-icon left size="xl" name="add_circle_outline" />
         <div>Créer un Pokémon</div>
       </q-btn>
     </div>
-    
+
     <q-banner v-if="success" inline-actions class="q-mb-lg text-white bg-green">
       <div class="text-h6">
         <q-icon left size="md" name="check_circle" />
@@ -62,18 +56,29 @@ onMounted(() => {
 
     <!-- Card list -->
     <div class="" v-for="(item, index) in pokemons" :key="index">
-      <q-card class="q-mb-sm">
+      <q-card
+        class="q-mb-sm"
+        :class="{ 'bg-green-3': item.is_owned, 'bg-grey-4': !item.is_owned }"
+      >
         <div class="flex justify-between">
           <q-card-section class="flex-auto">
-            <div class="text-h4">{{ item.name }}</div>
-            <div class="text-subtitle2">{{ item.pokemon_type_object.name }}</div>
+            <div class="text-h4">
+              {{ item.is_owned ? "✔️" : "" }} {{ item.name }}
+            </div>
+            <div class="text-subtitle2">
+              {{ item.pokemon_type_object.name }}
+            </div>
+            <div class="text-subtitle2"></div>
           </q-card-section>
 
           <div class="flex normal-btn-size">
             <q-btn
               color="red"
               push
-              @click="showDelDialog = true; removeItem = item;"
+              @click="
+                showDelDialog = true;
+                removeItem = item;
+              "
               class="q-ma-xs"
               dense
             >
@@ -99,11 +104,17 @@ onMounted(() => {
           </div>
         </q-card-section>
 
-        <hr>
+        <hr />
 
         <q-card-section>
-          <p>Souhaitez-vous réellement supprimer le pokémon <span class="text-teal">{{ removeItem.name }}</span></p>
-          <p>Si vous le supprimez, toutes les données associées le seront également.</p>
+          <p>
+            Souhaitez-vous réellement supprimer le pokémon
+            <span class="text-teal">{{ removeItem.name }}</span>
+          </p>
+          <p>
+            Si vous le supprimez, toutes les données associées le seront
+            également.
+          </p>
           <!-- TODO When game implemented : change text to specify the refund the players and remove it -->
           <p class="text-red">
             <q-icon name="crisis_alert" size="sm" />
@@ -111,14 +122,18 @@ onMounted(() => {
           </p>
         </q-card-section>
 
-        <hr>
-        
+        <hr />
+
         <q-card-actions align="right">
           <q-btn color="blue-grey" v-close-popup>
             <q-icon left size="xs" name="cancel" />
             Annuler
           </q-btn>
-          <q-btn color="red" @click="removePokemon(removeItem.id)" v-close-popup>
+          <q-btn
+            color="red"
+            @click="removePokemon(removeItem.id)"
+            v-close-popup
+          >
             <q-icon left size="xs" name="delete_outline" />
             Supprimer
           </q-btn>
