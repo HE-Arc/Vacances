053e0e4a92b3ae6cddc0f9a7c1ccdde5dc203c02
Jonas Allemann <jonas.allemann@he-arc.ch>
2023-04-24 00:03:48 +0200

    feat: tabs displayed according logged (#56)

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,32 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const user = ref(null);
+const isLogged = ref(false);
+
+const fetchConnected = async () => {
+  user.value = (await axios.get("users/current/")).data;
+  isLogged.value = user.value !== null;
+  console.log("logged = " + isLogged.value);
+  console.log(user.value);
+};
+
+onMounted(() => {
+  fetchConnected();
+});
+</script>
 
 <template>
   <q-header reveal elevated class="bg-grey-10 text-white" height-hint="98">
     <q-toolbar>
       <q-toolbar-title>Vacances ?</q-toolbar-title>
+
+      <q-separator vertical />
+
+      <div v-if="isLogged" class="q-ml-auto">
+        <q-toolbar-title>{{ user.username }}</q-toolbar-title>
+      </div>
     </q-toolbar>
 
     <q-tabs align="left">
@@ -11,25 +34,31 @@
         <q-icon name="cottage" />
         Accueil
       </q-route-tab>
-      <q-route-tab :to="{ name: 'pokemons' }">
+      <q-route-tab v-if="isLogged" :to="{ name: 'pokemons' }">
         <q-icon name="auto_stories" />
         Pokédex
       </q-route-tab>
-      <q-route-tab :to="{ name: 'users' }">
-        Se connecter
-      </q-route-tab>
-      <q-route-tab :to="{ name: 'users.logout' }">
-        Se déconnecter
-      </q-route-tab>
-      <q-route-tab :to="{ name: 'daycare' }">
+      <q-route-tab v-if="isLogged" :to="{ name: 'daycare' }">
         <q-icon name="grass" />
         Pension
       </q-route-tab>
-      <q-route-tab :to="{ name: 'shop' }">
+      <q-route-tab v-if="isLogged" :to="{ name: 'shop' }">
         <q-icon name="storefront" />
         Magasin
       </q-route-tab>
+
+      <q-route-tab v-if="isLogged" :to="{ name: 'users.logout' }">
+        <q-icon name="logout" />
+        Deconnexion
+      </q-route-tab>
+
+      <q-route-tab v-if="!isLogged" :to="{ name: 'users' }">
+        <q-icon name="login" />
+        Connexion
+      </q-route-tab>
     </q-tabs>
+
+    <q-space />
   </q-header>
 </template>
 
